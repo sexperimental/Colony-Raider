a10b823bd449080025eacfaad8812d7dcaefecb6
experimental <65273445+sexperimental@users.noreply.github.com>
2025-02-01 22:00:21 +0900

    Update Colony.py

--- Colony.py
+++ Colony.py
@@ -9,7 +9,7 @@ def randomword(length):
 
 url = "https://minecraft-mcworld.com/wp-comments-post.php"
 
-id = input("投稿IDを入力してください: ")
+id = input("URLにある数字を入力してください: ")
 comment = input("コメントしたい文字を入力してください: ")
 count = int(input("何個のコメントを送信しますか: "))
 
@@ -23,30 +23,10 @@ try:
     for i in range(count):
         data = {
             "comment": comment + "_" + randomword(5) + f"   私のMCIDはWater6528です。",
-            "author": "",
-            "email": "",
-            "url": "",
-            "submit": "コメントを送信",
             "comment_post_ID": id,
-            "comment_parent": "0",
-            "akismet_comment_nonce": "aa3d2604ca",
-            "ak_hp_textarea": "",
-            "ak_js": "1732444997676",
-            "ak_bib": "1732445006725",
-            "ak_bfs": "1732445009032",
-            "ak_bkpc": "5",
-            "ak_bkp": "137;74,771;147,93;91,80;78,63;","ak_bmc": "64;96,4551;85,550;97,139;47,2346;","ak_bmcc": "5",
-            "ak_bmk": "",
-            "ak_bck": "1;1",
-            "ak_bmmc": "5",
-            "ak_btmc": "0",
-            "ak_bsc": "6",
-            "ak_bte": "",
-            "ak_btec": "0",
-            "ak_bmm": "823,9257;810,476;592,10862;1293,10912;19,11304;",
         }
 
-        sleep_time = random.uniform(15, 17)
+        sleep_time = random.uniform(15, 15)
         time.sleep(sleep_time)
 
         try:
